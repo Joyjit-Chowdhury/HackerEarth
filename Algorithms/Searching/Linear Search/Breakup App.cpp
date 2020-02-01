658f58ade158e2b6b23732462d7de06dfd59aff4
Joyjit Chowdhury <53374920+Joyjit-Chowdhury@users.noreply.github.com>
2020-02-01 21:25:30 +0600

    Add files via upload

---
+++ Algorithms/Searching/Linear Search/Breakup App.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+
+    int n, count_19=0, count_20=0;
+    cin>>n;
+    while(n--)
+    {
+        string s;
+        getline(cin,s);
+        if(s[0]=='G')
+        {
+            for(int i = 0; s[i] != '\0'; i++)
+            {
+                if(s[i] == '1' && s[i+1] == '9' || s[i] == '2' && s[i+1] == '0')
+                {
+                    count_19 += 2;
+                }
+                else if((s[i] >= '0' && s[i] <= '9' && s[i+1] >= '0' && s[i+1] <= '9')
+                        &&! (s[i] == '1' && s[i+1] == '9' || s[i] == '2' && s[i+1] == '0'))
+                {
+                    count_20 += 2;
+                }
+            }
+        }
+        if(s[0] == 'M')
+        {
+
+            for(int i = 0; s[i] != '\0'; i++)
+            {
+                if(s[i] == '1' && s[i+1] =='9' || s[i] == '2' && s[i+1] == '0')
+                {
+                    count_19++;
+                }
+                else if((s[i] >= '0' && s[i] <= '9' && s[i+1] >= '0' && s[i+1] <= '9')
+                        &&!(s[i] == '1' && s[i+1] == '9' || s[i] == '2' && s[i+1] == '0'))
+                {
+                    count_20++;
+                }
+            }
+        }
+    }
+    if(count_19 > count_20)
+    {
+        cout <<"Date";
+    }
+    else
+    {
+        cout<<"No Date";
+    }
+    return 0;
+}
