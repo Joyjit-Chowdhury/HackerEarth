2296f26fe172dee2208d25490b2466592fe40355
Joyjit Chowdhury <53374920+Joyjit-Chowdhury@users.noreply.github.com>
2020-02-05 02:02:04 +0600

    Add files via upload

---
+++ Basic Programming/Input-Output/Basics of Input-Output.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    string s;
+    cin >> s;
+    
+    cout << (n * 2) << endl;
+    cout << s;
+    
+    return 0;
+}
